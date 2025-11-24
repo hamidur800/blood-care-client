9efbdc9d24f331407686ebafad3f17b84c52a5b1
hamidur800 <hamidur800t@gmail.com>
2025-11-24 22:39:50 +0600

    Blood-Care-frist_Commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      {/* Hero section st */}
+      <section>
+        <div className=""></div>
+      </section>
+      {/* Hero section end */}
+    </div>
+  );
+}
---
+++ src/proxy.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function proxy(request) {
+  return NextResponse.redirect(new URL("/", request.url));
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request) { ... }
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/DetailsPage/:path*",
+  //   matcher: "/login/:path*",
+};
